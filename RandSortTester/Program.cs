87ca40fd4173e01c205309cd0162cb33570759f8
criticalsession <criticalsession@protonmail.ch>
2024-09-05 18:35:44 +0200

    rename file and folder structure

--- RandSortTester/Program.cs
+++ RandSortTester/Program.cs
@@ -8,7 +8,7 @@ namespace RandSortTester {
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            int size = 500, chunkSize = 200;
+            int size = 100, chunkSize = 200;
 
             int[] data = new int[size];
             for (int i = 0; i < size; i++) {
